4b85a24fec2541f50452d8284e7b271c753d9891
gonzalezcreative <Hello@gonzalezcreative.com>
2024-11-20 00:59:22 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# v1g
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/gonzalezcreative/v1g)
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'firebase': ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+});
